greyscaled/dotfiles-archived

commit 02f4c75b44fbd1ddb91aceee155aa5f3b3be6eac

Add apt upgrade aliases

.bash_aliases
@@ -2,4 +2,6 @@ alias cgroot='cd $(groot)'
 alias chrome='google-chrome'
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias docker-bash='docker exec -it $(docker ps -q) bash'
-alias docker-clean-img='docker rmi $(docker images --filter "dangling=true" -q --no-trunc)'
+alias docker-clean-img='docker rmi $(docker images --filter "dangling=true" -q --no-trunc)i'
+alias upgradable='sudo apt list --upgradable'
+alias upgrade='sudo apt upgrade'

commit 678a3cf5ba93ba385db866853b4875d50ec9913c

Fix bug introduced in 02f4c75b

.bash_aliases
@@ -2,6 +2,6 @@ alias cgroot='cd $(groot)'
 alias chrome='google-chrome'
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias docker-bash='docker exec -it $(docker ps -q) bash'
-alias docker-clean-img='docker rmi $(docker images --filter "dangling=true" -q --no-trunc)i'
+alias docker-clean-img='docker rmi $(docker images --filter "dangling=true" -q --no-trunc)'
 alias upgradable='sudo apt list --upgradable'
 alias upgrade='sudo apt upgrade'